364f310d3da43a026a0f60445ab3d70a30088a7b
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2020-12-14 12:08:03 -0500

    refactor socials

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -9,6 +9,27 @@ let docs_open = false;
 let mobile_open = false;
 let community_open = false;
 
+const socials = [
+  {
+    name: "Telegram",
+    href: "https://t.me/pangolindex",
+    d:
+      "M23.91 3.79L20.3 20.84c-.25 1.21-.98 1.5-2 .94l-5.5-4.07-2.66 2.57c-.3.3-.55.56-1.1.56-.72 0-.6-.27-.84-.95L6.3 13.7.85 12c-1.18-.35-1.19-1.16.26-1.75l21.26-8.2c.97-.43 1.9.24 1.53 1.73z",
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/pangolindex",
+    d:
+      "M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z",
+  },
+  {
+    name: "GitHub",
+    href: "https://github.com/pangolindex",
+    d:
+      "M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12",
+  },
+];
+
 function handleKeydown({key}) {
   if (key === "Escape") {
     tools_open = false;
@@ -290,39 +311,18 @@ onMount(async () => {
         </div>
       </nav>
       <div class="hidden justify-end items-center space-x-4 md:flex md:flex-1 lg:w-0">
-        <a
-          class="text-gray-400 transition-colors duration-200 hover:text-gray-500"
-          href="https://t.me/pangolindex"
-        >
-          <span class="sr-only">Pangolin on Telegram</span>
-          <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24">
-            <path
-              d="M23.91 3.79L20.3 20.84c-.25 1.21-.98 1.5-2 .94l-5.5-4.07-2.66 2.57c-.3.3-.55.56-1.1.56-.72 0-.6-.27-.84-.95L6.3 13.7.85 12c-1.18-.35-1.19-1.16.26-1.75l21.26-8.2c.97-.43 1.9.24 1.53 1.73z"
-            ></path>
-          </svg>
-        </a>
-        <a
-          class="text-gray-400 transition-colors duration-200 hover:text-gray-500"
-          href="https://twitter.com/pangolindex"
-        >
-          <span class="sr-only">Pangolin on Twitter</span>
-          <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24">
-            <path
-              d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"
-            ></path>
-          </svg>
-        </a>
-        <a
-          class="text-gray-400 transition-colors duration-200 hover:text-gray-500"
-          href="https://github.com/pangolindex"
-        >
-          <span class="sr-only">Pangolin on GitHub</span>
-          <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24">
-            <path
-              d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
-            ></path>
-          </svg>
-        </a>
+        {#each socials as {name, href, d}}
+          <a
+            class="text-gray-400 transition-colors duration-200 hover:text-gray-500"
+            href="{href}"
+            target="_blank"
+          >
+            <span class="sr-only">Pangolin on {name}</span>
+            <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24">
+              <path d="{d}"></path>
+            </svg>
+          </a>
+        {/each}
       </div>
     </div>
   </div>
@@ -476,24 +476,11 @@ onMount(async () => {
         </div>
         <div class="py-6 px-5 space-y-6">
           <div class="grid grid-cols-2 gap-y-4 gap-x-8">
-            <a
-              href="https://t.me/pangolindex"
-              class="text-base font-medium text-gray-900 hover:text-gray-700"
-            >
-              Telegram
-            </a>
-            <a
-              href="https://twitter.com/pangolindex"
-              class="text-base font-medium text-gray-900 hover:text-gray-700"
-            >
-              Twitter
-            </a>
-            <a
-              href="https://github.com/pangolindex"
-              class="text-base font-medium text-gray-900 hover:text-gray-700"
-            >
-              GitHub
-            </a>
+            {#each socials as {href, name}}
+              <a href="{href}" class="text-base font-medium text-gray-900 hover:text-gray-700">
+                {name}
+              </a>
+            {/each}
           </div>
         </div>
       </div>
