7f64e51912f279132c81815ef08370a6fdad26ee
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2021-01-04 10:48:28 -0500

    add tutorials link

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -154,6 +154,26 @@ onMount(async () => {
                       <p class="text-base font-medium text-gray-900">Litepaper</p>
                     </div>
                   </a>
+
+                  <a href="#" class="flex items-start p-3 -m-3 rounded-lg cursor-not-allowed">
+                    <svg
+                      class="flex-shrink-0 w-6 h-6 text-gray-600"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"
+                      ></path>
+                    </svg>
+                    <div class="ml-4">
+                      <p class="text-base font-medium text-gray-900">Tutorials (Coming Soon)</p>
+                    </div>
+                  </a>
                 </div>
               </div>
             </div>
@@ -280,6 +300,24 @@ onMount(async () => {
                   ></path>
                 </svg>
                 <span class="ml-3 text-base font-medium text-gray-900">Litepaper</span>
+              </a>
+
+              <a href="#" class="flex items-center p-3 -m-3 rounded-md cursor-not-allowed">
+                <svg
+                  class="flex-shrink-0 w-6 h-6 text-gray-600"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"
+                  ></path>
+                </svg>
+                <span class="ml-3 text-base font-medium text-gray-900">Tutorials (Coming Soon)</span>
               </a>
             </nav>
           </div>
