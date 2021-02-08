8fe447a6a0e251c50d468fe61df3e9c9ea3df7d2
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2021-02-08 18:40:18 -0500

    bump deps

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,16 @@
+const {orange} = require("tailwindcss/colors");
+
 module.exports = {
   purge: {
     mode: "all",
     content: ["./src/**/*.{html,svelte}"],
   },
   plugins: [require("@tailwindcss/typography")],
+  theme: {
+    extend: {
+      colors: {
+        orange,
+      },
+    },
+  },
 };
