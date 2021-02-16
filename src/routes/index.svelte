dee2d1afa912a1aa82ec3cf40692866b33dd6d65
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2021-02-16 10:19:06 -0500

    add FAQ link to homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -139,6 +139,11 @@
         class="flex-none py-3 px-6 w-full text-lg font-semibold leading-6 text-gray-900 bg-white rounded-xl border border-gray-900 transition-colors duration-200 hover:text-gray-700 hover:bg-gray-50 hover:border-gray-700 focus:outline-none focus:ring-gray-900 focus:ring-offset-white focus:ring-offset-2 focus:ring-2 sm:w-auto"
         href="/litepaper">Litepaper</a
       >
+
+      <a
+        class="flex-none py-3 px-6 w-full text-lg font-semibold leading-6 text-gray-900 bg-white rounded-xl border border-gray-900 transition-colors duration-200 hover:text-gray-700 hover:bg-gray-50 hover:border-gray-700 focus:outline-none focus:ring-gray-900 focus:ring-offset-white focus:ring-offset-2 focus:ring-2 sm:w-auto"
+        href="/faq">FAQ</a
+      >
     </div>
   </div>
 
