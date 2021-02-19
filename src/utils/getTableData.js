a46891e2ec050868e3601c6e4f53b587e070f5d5
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2021-02-18 19:44:22 -0500

    update table to use pangolin analytics

---
+++ src/utils/getTableData.js
@@ -0,0 +1,116 @@
+import {TABLE_ADDRESSES, PANGOLIN_SUBGRAPH_URL, BLOCK_SUBGRAPH_URL} from "./constants";
+
+const TokenFields = `
+  fragment TokenFields on Token {
+    id
+    name
+    symbol
+    derivedETH
+    tradeVolume
+  }
+`;
+
+async function getBlockFromTimestamp(timestamp) {
+  const query = `
+    query blocks($timestampFrom: Int!, $timestampTo: Int!) {
+      blocks(
+        first: 1
+        orderBy: timestamp
+        orderDirection: asc
+        where: { timestamp_gt: $timestampFrom, timestamp_lt: $timestampTo }
+      ) {
+        id
+        number
+        timestamp
+      }
+    }
+  `;
+
+  const req = await fetch(BLOCK_SUBGRAPH_URL, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({
+      query,
+      variables: {
+        timestampFrom: timestamp,
+        timestampTo: timestamp + 60 * 60 * 24 * 7,
+      },
+    }),
+  });
+
+  const result = await req.json();
+
+  return result?.data?.blocks?.[0]?.number;
+}
+
+async function getCurrentTokens() {
+  const query = `
+    ${TokenFields}
+    query tokens {
+      tokens(first: 200, orderBy: tradeVolumeUSD, orderDirection: desc) {
+        ...TokenFields
+      }
+    }
+  `;
+
+  const req = await fetch(PANGOLIN_SUBGRAPH_URL, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({query}),
+  });
+
+  const result = await req.json();
+
+  return result?.data?.tokens;
+}
+
+async function getHistoryTokens() {
+  const block = await getBlockFromTimestamp(Math.floor(new Date().getTime() / 1000) - 86400);
+
+  const query = `
+    ${TokenFields}
+    query tokens {
+      tokens(block: {number: ${block}} first: 200, orderBy: tradeVolumeUSD, orderDirection: desc) {
+        ...TokenFields
+      }
+    }
+  `;
+
+  const req = await fetch(PANGOLIN_SUBGRAPH_URL, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({query}),
+  });
+
+  const result = await req.json();
+
+  return result?.data?.tokens;
+}
+
+export async function getTableData() {
+  const currentTokens = await getCurrentTokens();
+  const historyTokens = await getHistoryTokens();
+
+  return Promise.all(
+    TABLE_ADDRESSES.map((addr) => {
+      const {name, symbol, derivedETH: now, tradeVolume} = currentTokens.find(
+        ({id}) => id.toLowerCase() == addr.toLowerCase(),
+      );
+
+      const {derivedETH: history, tradeVolume: historyVolume} = historyTokens.find(
+        ({id}) => id.toLowerCase() == addr.toLowerCase(),
+      );
+
+      return {
+        addr,
+        name,
+        symbol,
+        price: {
+          now,
+          history,
+        },
+        volume: tradeVolume - historyVolume,
+      };
+    }),
+  );
+}
