572fd01d6dcd17f0447f53b602fcfdb9c734502f
Dhruba Basu <7675102+dhrubabasu@users.noreply.github.com>
2021-05-10 17:07:21 -0400

    add max apr (#14)

--- src/lib/utils/getTotalStats.js
+++ src/lib/utils/getTotalStats.js
@@ -1,5 +1,14 @@
+import {STAKING_ADDRESSES} from "./constants";
+
 export async function getTotalStats() {
+  const aprs = await Promise.all(
+    STAKING_ADDRESSES.map(async (address) =>
+      parseFloat(await (await fetch(`https://api.pangolin.exchange/pangolin/apr/${address}`)).text()),
+    ),
+  );
+
   return {
+    maxAPR: Math.max(...aprs),
     totalVolume: parseFloat(await (await fetch("https://api.pangolin.exchange/png/total-volume")).text()),
     totalLiquidity: parseFloat(await (await fetch("https://api.pangolin.exchange/png/tvl")).text()),
   };
--- svelte.config.cjs
+++
@@ -1,8 +0,0 @@
-const pkg = require('./package.json');
-
-/** @type {import('@sveltejs/kit').Config} */
-module.exports = {
-  kit: {
-    adapter: require('@sveltejs/adapter-static')(),
-  }
-};
