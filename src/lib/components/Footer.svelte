2291267cd5d93b726a905e474ae7d811aeeea3e5
Brandon <brandonmino@bellsouth.net>
2021-06-17 17:16:50 -0400

    Add Static Footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,1 +1,27 @@
-<footer class="mt-4 mb-6 text-center text-gray-500">Made with ❤️</footer>
+<footer class="mt-4 mb-6 text-center text-gray-500">
+    <div class="grid grid-cols-3 gap-y-5 sm:gap-y-0">
+        <div class="order-2 sm:order-1 col-span-3 sm:col-span-1">
+            <div><strong>Partners</strong></div>
+            <ul>
+                <li><a href="https://www.avax.network/" target="_blank">Powered by Avalanche</a></li>
+            </ul>
+        </div>
+        <div class="order-1 sm:order-2 col-span-3 sm:col-span-1">
+            <div><strong>Find us outside Avalanche</strong></div>
+            <a href="https://www.coingecko.com/en/coins/pangolin" target="_blank">
+                <img src="/coingecko.png" class="icon" alt="CoinGecko logo" />
+            </a>
+            <a href="https://www.gate.io/en/trade/PNG_USDT" target="_blank">
+                <img src="/gateio.png" class="icon" alt="Gate.io logo" />
+            </a>
+        </div>
+        <div class="order-3 sm:order-3 col-span-3 sm:col-span-1">
+            <div><strong>Community Elected Core Team</strong></div>
+            <ul>
+                <li><a href="mailto:jtrollip@protonmail.com">Justin (Project Manager)</a></li>
+                <li><a href="mailto:pangolin.exchange@gmail.com">Leo (Community Manager)</a></li>
+                <li><a href="mailto:bmino1234@gmail.com">Brandon (Lead Developer)</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
