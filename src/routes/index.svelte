f807c0ffe78c1a3036c665d0bd36fd4d3cf4edc8
best-coder-NA <best-coder-NA@users.noreply.github.com>
2021-07-09 16:50:48 +0000

    [ci] yarn format

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -223,7 +223,12 @@
       </div>
       <div class="mr-5 ml-5">
         <a href="https://www.poolz.finance/" target="_blank">
-          <img class="object-scale-down" src="/poolz.png" alt="Poolz" style="display: inline-block; height: 80px; margin-top: -15px" />
+          <img
+            class="object-scale-down"
+            src="/poolz.png"
+            alt="Poolz"
+            style="display: inline-block; height: 80px; margin-top: -15px"
+          />
         </a>
       </div>
       <div>
