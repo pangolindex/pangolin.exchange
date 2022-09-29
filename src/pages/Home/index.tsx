52568b46f9a679dbe99ebdcac7e0e23d95194b19
Pedro <pedroantonioppms@gmail.com>
2022-09-29 10:35:21 -0400

    Fix text issues

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Text } from "@pangolindex/components";
-import { Main, Root } from "./styled";
+import { CTAText, Main, Root } from "./styled";
 import homeImage from "src/assets/images/home.png";
 
 export default function Home() {
@@ -7,15 +7,22 @@ export default function Home() {
     <Root>
       <Main>
         <Box>
-          <Text color="primary" fontWeight="700" fontSize="70px">
-            Your Gateway to A Multi-Chain Future
-          </Text>
-          <Text color="text1" fontSize="18px">
-            Pangolin is a multichain decentralized cryptocurrency exchange
-            (DEX). Through advanced tooling, deep liquidity, and smooth
-            onboarding, Pangolin offers easy swaps and great tools for projects
-            and traders.
-          </Text>
+          <Box
+            height="100%"
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+          >
+            <Text color="primary" fontWeight="700" fontSize="50px">
+              Your Gateway to A Multi-Chain Future
+            </Text>
+            <Text color="text1" fontSize="18px">
+              Pangolin is a multichain decentralized cryptocurrency exchange
+              (DEX). Through advanced tooling, deep liquidity, and smooth
+              onboarding, Pangolin offers easy swaps and great tools for
+              projects and traders.
+            </Text>
+          </Box>
         </Box>
         <img src={homeImage} alt="Charts" />
       </Main>
@@ -25,17 +32,23 @@ export default function Home() {
         padding="40px"
         justifyContent="space-between"
         borderRadius="20px"
+        alighItems="center"
       >
-        <Text color="text1" fontSize="36px">Try our one of a kind multi-chain experience in all Defi!</Text>
+        <CTAText>
+          Try our one of a kind multi-chain experience in all Defi!
+        </CTAText>
         <Button
           as="a"
           variant="primary"
           color="eerieBlack"
           target="_self"
           width="max-content"
-          padding="20px 60px"
+          padding="0px 60px"
+          href="https://app.pangolin.exchange"
         >
-          Lauch App
+          <Text color="eerieBlack" fontSize="18px">
+            Lauch App
+          </Text>
         </Button>
       </Box>
     </Root>
--- src/pages/Home/styled.tsx
+++ src/pages/Home/styled.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@pangolindex/components";
+import { Box, Text } from "@pangolindex/components";
 import styled from "styled-components";
 
 export const Root = styled(Box)`
@@ -12,4 +12,12 @@ export const Main = styled(Box)`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 80px;
+  height: 55vh;
+`
+
+export const CTAText = styled(Text)`
+  color: ${({theme}) => theme.text1};
+  font-size: 26px;
+  height: fit-content;
+  align-self: center;
 `
