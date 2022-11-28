3f5a1bf9b890158535ab2cadef97da86048bb0c1
Pedro Antonio <37405304+prodesert22@users.noreply.github.com>
2022-11-28 00:35:02 -0400

    fix: fetch partners & chains data from directus (#67)
    * feat: use querystring to stringify the query
    
    * feat: get live chains from directus

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,22 +1,72 @@
+import qs from 'qs'
 import { useQuery } from 'react-query'
 import { directusPangoAPI } from 'src/constants'
 
-interface Partner {
+interface DirectusBase {
   id: number
-  status: string
   date_created: string
+}
+
+interface Info extends DirectusBase {
   logo: string
   name: string
   url: string
 }
+interface Partner extends Info {
+  status: string
+}
+
+interface Chain extends Info {
+  pangolin_is_live: boolean
+}
 
 export function useGetPartners() {
+  const query = qs.stringify(
+    {
+      filter: {
+        status: {
+          _eq: 'published'
+        }
+      }
+    },
+    {
+      encodeValuesOnly: true
+    }
+  )
+
   return useQuery(
     'pango-partners',
     async () => {
-      const response = await directusPangoAPI.get('/items/Partners')
-      const data = response.data
-      return (data.data as Partner[]).filter(partner => partner.status.toLowerCase() === 'published')
+      const response = await directusPangoAPI.get(`items/Partners?${query}`)
+      const data = response.data.data as Partner[]
+      return data
+    },
+    {
+      cacheTime: 1 * 60 * 60 * 1000 // 1 hour
+    }
+  )
+}
+
+export function useGetLiveChains() {
+  const query = qs.stringify(
+    {
+      filter: {
+        pangolin_is_live: {
+          _eq: true
+        }
+      }
+    },
+    {
+      encodeValuesOnly: true
+    }
+  )
+
+  return useQuery(
+    'pango-live-chains',
+    async () => {
+      const response = await directusPangoAPI.get(`/items/chains?${query}`)
+      const data = response.data.data as Chain[]
+      return data
     },
     {
       cacheTime: 1 * 60 * 60 * 1000 // 1 hour
