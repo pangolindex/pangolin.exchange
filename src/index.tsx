527dd0a13d89016ad0d20e65ed067c90cf960bb1
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2023-06-04 20:26:28 +0530

    fix: replace package lock with yarn lock

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import store, { AppContext } from './state'
 import ThemeProvider, { FixedGlobalStyle, ThemedGlobalStyle } from './theme'
 import { Web3Provider } from '@ethersproject/providers'
 import { Web3ReactProvider, useWeb3React } from '@web3-react/core'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 const getLibrary = (provider: any): Web3Provider => {
   const library = new Web3Provider(provider, 'any')
@@ -16,18 +17,29 @@ const getLibrary = (provider: any): Web3Provider => {
   return library
 }
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 1000 * 60,
+      refetchOnWindowFocus: false
+    }
+  }
+})
+
 const ComponentThemeProvider = () => {
   const theme = useContext(ThemeContext)
   const { library, account, chainId } = useWeb3React<Web3Provider>()
 
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <PangolinProvider library={library} chainId={chainId} account={account} theme={theme as any}>
-        <FixedGlobalStyle />
-        <ThemedGlobalStyle />
-        <App />
-      </PangolinProvider>
-    </Web3ReactProvider>
+    <QueryClientProvider client={queryClient}>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <PangolinProvider library={library} chainId={chainId} account={account} theme={theme as any}>
+          <FixedGlobalStyle />
+          <ThemedGlobalStyle />
+          <App />
+        </PangolinProvider>
+      </Web3ReactProvider>
+    </QueryClientProvider>
   )
 }
 
