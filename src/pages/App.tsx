4ff02ed80eb0125c9793c24f6c065b7cf89a4dc4
Bilal Corbacioglu <bilalcorbacioglu@gmail.com>
2023-07-17 18:11:12 +0100

    feat: wallet modal integration removed

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,35 +1,13 @@
-import React, { Suspense, useCallback } from 'react'
+import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import Layout from '../layout'
 import Intercom from 'src/intercom'
-import { WalletModal } from '@pangolindex/components'
-import { useWeb3React } from '@web3-react/core'
-import { Web3Provider } from '@ethersproject/providers'
 
 export default function App() {
-  const [walletModalActive, setWalletModalActive] = React.useState<boolean>(false)
-
-  const handleMessengerOpen = useCallback(() => {
-    setWalletModalActive(true)
-  }, [setWalletModalActive])
-
-  const { account } = useWeb3React<Web3Provider>()
-
   return (
     <Suspense fallback={null}>
-      <Intercom appID={'uj2kwt4z'} walletAddress={account as string} onMessengerOpen={handleMessengerOpen} />
-      {!account && (
-        <WalletModal
-          open={walletModalActive}
-          closeModal={() => {
-            setWalletModalActive(false)
-          }}
-          onWalletConnect={wallet => {
-            console.log(wallet)
-          }}
-        />
-      )}
+      <Intercom appID={'uj2kwt4z'} />
       <Layout>
         <Router>
           <Routes>
